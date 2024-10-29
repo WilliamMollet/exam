83e5b72eaac4100cae34a3ca954235652c19b15a
WilliamMollet <williammdev@gmail.com>
2024-10-29 12:41:37 +0100

    5th Commit (Final I hope)

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Démarrer Nginx en avant-plan pour maintenir le conteneur actif
-nginx -g "daemon off;"
-
 # Démarrer MySQL
 service mysql start
 
 # Démarrer PHP-FPM (pour gérer les requêtes PHP dans Nginx)
 service php7.3-fpm start
+chmod 777 /var/run/php/php7.3-fpm.sock
 
+# Démarrer Nginx en avant-plan pour maintenir le conteneur actif
+nginx -g 'daemon off;'
